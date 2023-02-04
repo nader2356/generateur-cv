774e81f43a38476acee466223531f9142dbc1232
nader2356 <naderabdessaied8@gmail.com>
2023-02-04 19:34:27 +0100

    configuration_tailwind_css

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'sm': "480px",
+      'md': "768px",
+      'lg': "976px",
+      'xl': "1440px",
+      '2xl': "1536px",
+    },
+    colors: {
+      blue: "#1fb6ff",
+      white: '#ffffff',
+      purple: "#7e5bef",
+      "text_panel" : "#38383d",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      'barckground_textarea': "#f2f5fa",
+      'border_color' : '#d9d9d9',
+      'panel': 'rgba(0, 0, 0, 0.02)',
+      "gray-dark": "#273444",
+      black :"#000",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      red : "#cf5454"
+    },
+    extend: {
+      screens: {
+        "3xl": "1600px",
+      },
+     
+    },
+  },
+  plugins: [],
+};
