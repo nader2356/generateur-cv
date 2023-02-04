df3c337bb109b08f278cdcc56598f5dac78e035a
nader2356 <naderabdessaied8@gmail.com>
2023-02-04 19:39:03 +0100

    add_page_index

---
+++ src/Pages/Index.js
@@ -0,0 +1,13 @@
+
+
+const Index = () => {
+  return (
+    <div className="flex min-h-screen flex-col justify-center bg-[#ffff] py-[5%] sm:px-6 lg:px-8">
+      <div className="mb-auto   sm:max-w-8xl sm:mx-auto sm:w-full ">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Index;
